aefd81915a57d27d2839f1a3c585e0f303136722
Taras <sokha2411@gmail.com>
2023-07-05 18:14:36 +0300

    styles

--- src/app/components/quiz-card/quiz-card.component.ts
+++ src/app/components/quiz-card/quiz-card.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-quiz-card',
   templateUrl: './quiz-card.component.html',
-  styleUrls: ['./quiz-card.component.scss']
+  styleUrls: ['./quiz-card.component.scss'],
 })
 export class QuizCardComponent {
-
+  fn() {
+    console.log('aaa');
+  }
 }
